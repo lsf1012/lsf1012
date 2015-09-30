5c275dbe2f9076ace98a49ab6da8e98eff2c0315
lsf1012 <lsf1012@qq.com>
2015-09-30 13:10:22 +0800

    添加一个工程：Bmp转Jpg的小工具

---
+++ BMP2JPG_Tool/BMP2JPG_Tool/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace BMP2JPG_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "拖动",
+            "文件",
+            "到此",
+            "处"});
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(175, 424);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 94);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(231, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(175, 424);
+            this.listBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(423, 445);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMP&JPG图片批量转换_________CopyRight by lsf1012@qq.com";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ BMP2JPG_Tool/BMP2JPG_Tool/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace BMP2JPG_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private string _path = "";
+        private bool isHave = false;
+        private ArrayList files = new ArrayList();
+        private int Zi = 1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 500;
+            // Force the ToolTip text to be displayed whether or not the form is active.
+            toolTip1.ShowAlways = true;
+
+            // Set up the ToolTip text for the Button and Checkbox.
+            toolTip1.SetToolTip(this.listBox1, "将要转换的文件拖放到这里");
+            SetCtrlDragEvent(this.listBox1);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isHave)
+            {
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                listBox1.Items.Add("拖动");
+                listBox1.Items.Add("文件");
+                listBox1.Items.Add("到此");
+                listBox1.Items.Add("处");
+                return;
+            }
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\" + DateTime.Now.ToString("hhmmss_MMdd");
+           
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                other2jpg(_path + "\\" + listBox1.Items[i].ToString().ToLower(), folder);
+                listBox2.Items.Add(string.Format("{0:000}", i + 1).ToString() + ".jpg");
+            }
+            isHave = false;
+        }
+        public void bmp2jpg(string st1, string st2)
+        {
+            ImageCodecInfo[] vImageCodecInfos = ImageCodecInfo.GetImageEncoders();
+            Bitmap vBitmap = new Bitmap(st1);
+
+            foreach (ImageCodecInfo vImageCodecInfo in vImageCodecInfos)
+            {
+                if (vImageCodecInfo.FormatDescription.ToLower() == "jpeg")
+                {
+                    EncoderParameters vEncoderParameters = new EncoderParameters(1);
+                    vEncoderParameters.Param[0] = new EncoderParameter(
+                        System.Drawing.Imaging.Encoder.Quality, 75L);
+                    vBitmap.Save(st2, vImageCodecInfo, vEncoderParameters);
+                    break;
+                }
+            }
+        }
+        private void jpg2jpg(string str1, string folder, string filename)
+        {
+
+            if (!Directory.Exists(folder))
+            {
+                try { Directory.CreateDirectory(folder); }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("error:"+ex.Message);
+                }
+
+            }
+            FileInfo fileInfo = new FileInfo(str1);
+            double size = (double)fileInfo.Length;
+            int rat = Convert.ToInt32(1972864.0 / ((double)size) * 100);
+            // MessageBox.Show(rat.ToString());
+            if (rat < 100)
+            {
+                GetPicThumbnail(str1, folder + "\\" + filename, rat);
+            }
+            if (rat > 100) { File.Copy(str1, folder + "\\" + filename); }
+        }
+        private void other2jpg(string str1, string folder)
+        {
+
+            switch (str1.Substring(str1.Length - 4, 4))
+            {
+                case ".jpg":
+                    jpg2jpg(str1, folder, string.Format("{0:000}", Zi) + ".jpg");
+                    //jpg2jpg(str1, folder, Path.GetFileName(str1));
+                    Zi++;
+                    break;
+                case ".bmp":
+                    string tmp = Path.GetTempFileName() + ".jpg";
+                    bmp2jpg(str1, tmp);
+                    jpg2jpg(tmp, folder, string.Format("{0:000}", Zi) + ".jpg");
+                    // jpg2jpg(tmp, folder, Path.GetFileNameWithoutExtension(str1) + ".jpg");
+                    Zi++;
+                    break;
+            }
+
+        }
+        public void SetCtrlDragEvent(Control ctrl)
+        {
+            if (ctrl is ListBox)
+            {
+                ListBox tb = ctrl as ListBox;
+                tb.AllowDrop = true;
+                tb.DragEnter += (sender, e) =>
+                {
+                    e.Effect = DragDropEffects.Link;//拖动时的图标
+                };
+                tb.DragDrop += (sender, e) =>
+                {
+
+                    listBox1.Items.Clear();
+                    files.Clear();
+                    isHave = true;
+                    String[] fileNames = (String[])e.Data.GetData(DataFormats.FileDrop);
+                    // int j = ((ListBox)sender).Items.Count + 1;
+                    for (int i = 0; i < fileNames.Length; i++)
+                    {
+                        if (Path.GetExtension(fileNames[i].ToLower()) == ".jpg" || Path.GetExtension(fileNames[i].ToLower()) == ".bmp")
+                        {
+                            files.Add(fileNames[i]);
+                            // ((ListBox)sender).Items.Add(System.IO.Path.GetFileName(fileNames[i]));
+                        }
+                    }
+                    IComparer dateCompare = new DateCompare();
+                    files.Sort(dateCompare);
+                    foreach (string s in files)
+                    {
+                        listBox1.Items.Add(Path.GetFileName(s));
+                    }
+
+                    _path = System.IO.Path.GetDirectoryName(fileNames[0]);
+                };
+            }
+        }
+        public bool GetPicThumbnail(string sFile, string dFile, int flag)
+        {
+
+            System.Drawing.Image iSource = System.Drawing.Image.FromFile(sFile);
+            ImageFormat tFormat = iSource.RawFormat;
+            int sW = 0, sH = 0;
+            //按比例缩放
+            int dHeight = iSource.Height;
+            int dWidth = iSource.Width;
+            Size tem_size = new Size(iSource.Width, iSource.Height);
+            if (tem_size.Width > dHeight || tem_size.Width > dWidth) //将**改成c#中的或者操作符号
+            {
+                if ((tem_size.Width * dHeight) > (tem_size.Height * dWidth))
+                {
+                    sW = dWidth;
+                    sH = (dWidth * tem_size.Height) / tem_size.Width;
+                }
+                else
+                {
+                    sH = dHeight;
+                    sW = (tem_size.Width * dHeight) / tem_size.Height;
+                }
+            }
+
+            else
+            {
+                sW = tem_size.Width;
+                sH = tem_size.Height;
+            }
+            Bitmap ob = new Bitmap(dWidth, dHeight);
+            Graphics g = Graphics.FromImage(ob);
+            g.Clear(Color.WhiteSmoke);
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.DrawImage(iSource, new Rectangle((dWidth - sW) / 2, (dHeight - sH) / 2, sW, sH), 0, 0, iSource.Width, iSource.Height, GraphicsUnit.Pixel);
+            g.Dispose();
+            //以下代码为保存图片时，设置压缩质量
+            EncoderParameters ep = new EncoderParameters();
+            long[] qy = new long[1];
+            qy[0] = flag;//设置压缩的比例1-100
+            EncoderParameter eParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, qy);
+            ep.Param[0] = eParam;
+            try
+            {
+                ImageCodecInfo[] arrayICI = ImageCodecInfo.GetImageEncoders();
+                ImageCodecInfo jpegICIinfo = null;
+                for (int x = 0; x < arrayICI.Length; x++)
+                {
+                    if (arrayICI[x].FormatDescription.Equals("JPEG"))
+                    {
+                        jpegICIinfo = arrayICI[x];
+                        break;
+                    }
+                }
+                if (jpegICIinfo != null)
+                {
+                    ob.Save(dFile, jpegICIinfo, ep);//dFile是压缩后的新路径
+                }
+                else
+                {
+                    ob.Save(dFile, tFormat);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                iSource.Dispose();
+                ob.Dispose();
+            }
+        }
+    }
+    public class DateCompare : IComparer
+    {
+        int IComparer.Compare(Object x, Object y)
+        {
+            FileInfo fx = new FileInfo((string)x);
+            FileInfo fy = new FileInfo((string)y);
+            return fx.CreationTime.CompareTo(fy.CreationTime);
+        }
+    }
+}
